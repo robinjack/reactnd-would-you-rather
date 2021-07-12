879c6570395ba5b2dbf92b2e1744a48680e0928e
robinjack <robinmajack@gmail.com>
2021-07-12 15:50:35 +0100

    have added in the 404

---
+++ src/components/NotFound.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+
+
+
+
+class NotFound extends Component {
+
+    render() {
+        return (<h3> Question not found. Please try again!</h3>)
+    }
+}
+
+
+export default NotFound
